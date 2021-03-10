5eabe74ac421ee046cf602876b558b8fca67613c
dsurush <DavlatzodaS@HUMO.local>
2021-03-10 11:30:53 +0500

    fix sendIndividuloan func

--- core/services/kivaservice.go
+++ core/services/kivaservice.go
@@ -73,13 +73,13 @@ public ResponseEntity<?> sendPostIndividualLoan(IndividualLoan individualLoan) {
     }
  */
 
-func (receiver *KivaService) SendPostIndividualLoan(Body models.IndividualLoan) (err error, ok bool) {
+func (receiver *KivaService) SendPostIndividualLoan(Body models.IndividualLoan) (error, bool, models.IndividualLoan) {
 	client := &http.Client{}
-	var newBody models.Repayments
+	var newBody models.IndividualLoan
 	body, err := json.Marshal(Body)
 	if err != nil {
 		log.Printf("can't add to body, err is %err", err)
-		return err, true
+		return err, true, newBody
 	}
 	URL := `https://api.kivaws.org/v3/partner/63/loan_draft`
 	req, err := http.NewRequest(
@@ -87,24 +87,24 @@ func (receiver *KivaService) SendPostIndividualLoan(Body models.IndividualLoan) 
 	)
 	if err != nil {
 		log.Println("can't create Request", err)
-		return err, true
+		return err, true, newBody
 	}
 	req.Header.Add("Content-Type", "application/json; charset=utf-8")
 
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Printf("Can't send Request\n")
-		return err, true
+		return err, true, newBody
 	}
 	///json.NewDecoder(resp.Body).Decode()
 	log.Println(`Status Code = `, resp.StatusCode)
 	if resp.StatusCode == http.StatusOK {
-		return nil, false
+		return nil, false, newBody
 	}
 
 	err = json.NewDecoder(resp.Body).Decode(newBody)
 	if err != nil {
-
+		log.Println("invalid_json err is ", err)
 	}
-	return nil, true
+	return nil, true, newBody
 }
